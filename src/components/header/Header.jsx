37920fa31446a08ec4bd3d0b330884b3156b8b46
ava5241 <tamannahasannavila@gmail.com>
2025-05-19 06:30:08 +0600

    last

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar,Container,Nav,NavDropdown} from 'react-bootstrap'
 import './header.css'
+import { Link } from 'react-router-dom'
 import navi from "../../assets/nav1.png"
 
 function Header (){
@@ -13,8 +14,12 @@ function Header (){
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto a-in">
-          <Nav.Link className='in' href="#home">Home</Nav.Link>
-          <Nav.Link className='in' href="#link">About us</Nav.Link>
+          <Nav.Link className='in' href="#home" >
+            <Link to='/'>Home</Link>
+          </Nav.Link>
+          <Nav.Link className='in'>
+            <Link to='/about'>About Us</Link>
+          </Nav.Link>
           <Nav.Link className='in' href="#link">Features</Nav.Link>
           <Nav.Link className='in' href="#link">Pricing</Nav.Link>
           <Nav.Link className='in' href="#link">FAQ</Nav.Link>
